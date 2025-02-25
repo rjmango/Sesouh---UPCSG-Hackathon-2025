71cea7a1a7b4063f9bb91a2153198a557a7a6b81
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-02-25 11:02:16 +0800

    Added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <main>
-      <div className="bg-red-50 p-1 font-mono">Hello guyss</div>
+      <div className="bg-red-600 font-mono">Hello guyss</div>
     </main>
   );
 }
